3c6370c7d488c216c9d02dc01163a672a5626583
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-05 21:58:09 -0300

    feat: add refactor structure and add basic-info

--- server.go
+++ server.go
@@ -10,7 +10,8 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/mufasa-dev/cv-manager/graph"
+	"github.com/mufasa-dev/cv-manager/generated"
+	"github.com/mufasa-dev/cv-manager/resolvers"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
@@ -22,7 +23,7 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
